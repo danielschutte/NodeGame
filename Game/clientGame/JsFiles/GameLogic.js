cb55d35012097cb7e7cabf4f141f9c5c36a42113
ArjenAch <arjenachterveld@hotmail.com>
2017-03-16 09:55:50 +0100

    GameLogic

---
+++ Game/clientGame/JsFiles/GameLogic.js
@@ -0,0 +1,41 @@
+/**
+ * Created by arjen on 16-3-2017.
+ */
+var Players =[];
+
+// var player(id, name, color, xPos, yPos)   {
+//     this.id = id;
+//     this.xPos = xPos;
+//     this.yPos = yPos;
+//     this.name = name;
+// };
+
+//creates new player
+function CreateNewPlayer() {
+
+    Players.add(player);
+}
+
+function startGame() {
+
+    myGameArea.start();
+    player = new initPlayer(30,30,"red",10,120);
+}
+
+
+
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.frameNo = 0;
+        this.interval = setInterval(updateGameArea, 20);
+
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
