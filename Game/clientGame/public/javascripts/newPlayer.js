b9215ef2ece269454b7b431d51b77badfbfc1c59
danielschutte <dschutte@84-255.dhcp.windesheim.nl>
2017-03-20 14:14:34 +0100

    Color list Update

--- Game/clientGame/public/javascripts/newPlayer.js
+++ Game/clientGame/public/javascripts/newPlayer.js
@@ -1,6 +1,19 @@
 /**
  * Created by dschutte on 16-03-17.
  */
+
+var colors = {"colors" : [
+    {"name":"green", "hex":"hexcode"},
+    {"name":"red", "hex":"hexcode"},
+    {"name":"blue", "hex":"hexcode"}
+]};
+
+$.each(colors, function(i, item){
+    $('#colorList').append('<li>'+item.name+'</li>')
+});
+
+
+
 function dropDown(click){
     var x = document.getElementById("dropDown");
     x.classList.toggle("change");
