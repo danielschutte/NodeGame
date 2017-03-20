299af312c883ad35f3b1b48064d10604367ef0fe
danielschutte <dschutte@84-255.dhcp.windesheim.nl>
2017-03-20 16:17:32 +0100

    Color List updated

--- Game/clientGame/public/javascripts/newPlayer.js
+++ Game/clientGame/public/javascripts/newPlayer.js
@@ -3,18 +3,32 @@
  */
 
 var colors = {"colors" : [
-    {"name":"green", "hex":"hexcode"},
-    {"name":"red", "hex":"hexcode"},
-    {"name":"blue", "hex":"hexcode"}
+    {"id" : 0, "name":"green", "hex":"#7CFC00"},
+    {"id" : 1,"name":"red", "hex":"#ff0000"},
+    {"id" : 2,"name":"blue", "hex":"#0000ff"},
+    {"id" : 3,"name":"yellow", "hex":"#ffff00"},
+    {"id" : 4,"name":"pink", "hex":"#ff00ce"}
 ]};
 
-$.each(colors, function(i, item){
-    $('#colorList').append('<li>'+item.name+'</li>')
+
+$(function(){
+    $.each(colors.colors, function(i, item){
+        $('#colorList').append('<li id='+item.id+' style="color:'+item.hex+'" onclick="setColor()">'+item.name+'</li>');
+    });
 });
+
+
 
 
 
 function dropDown(click){
     var x = document.getElementById("dropDown");
     x.classList.toggle("change");
+}
+
+function setColor(click){
+    var x = document.getElementById("selectedColor");
+    $("#selectedColor").empty();
+    var y = document.getElementById(click.id);
+    x.append();
 }
