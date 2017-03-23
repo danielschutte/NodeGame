c54bd69a18fa517259cff8e438131695b548ac44
b93rn <lenardborn@gmail.com>
2017-03-23 11:59:21 +0100

    Created flash errors and some general routing stuff

--- Game/clientGame/bin/sockets.js
+++ Game/clientGame/bin/sockets.js
@@ -3,16 +3,23 @@
  */
 
 var socketio = require('socket.io');
+var app = require('../routes/game').app;
 var logic = require('./logic');
 
 // all socket server side logic will take place here
 module.exports.listen = function(server){
+
     io = socketio.listen(server);
 
+    //var players = [];
     io.on('connection', function(socket) {
-        var test = logic.createPlayerByName('lenard', 'lenard');
-        console.log('Hallo '+ test.name);
+        console.log('het werkt ');
+        // if(app.locals.player){
+        //     var player = app.locals.player;
+        //     app.locals.player = null;
+        //     players.add(player);
+        //     console.log(players);
+        // }
     });
-
     return io;
 };
--- Game/clientGame/routes/game.js
+++ Game/clientGame/routes/game.js
@@ -1,27 +1,41 @@
 var app = require('../app').app;
 var express = require('express');
+var logic = require('../bin/logic');
 var router = express.Router();
 
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-
-    res.render('gameField', { title: 'new player' ,customJs:'/Javascripts/GameLogic.js'});
+// Game routers
+router.get('/GameField', function(req, res, next) {
+    res.render('GameField', {title: 'Create a new player', customJs: '/Javascripts/playerSelection.js'});
 });
 
-router.get('/NewPlayer', function(req, res, next) {
+// New player
+router.get('/newPlayer', function(req, res, next) {
     res.render('newPlayer', {title: 'Create a new player', customJs: '/Javascripts/playerSelection.js'});
 });
 
-router.post('/NewPlayer', function(req, res) {
-    if(req.body.playerName == ''){
-        res.render('NewPlayer', {title: 'Lets play'});
-    }
-    else
-    {
-        res.redirect('./GameField');
-    }
+// Player has to choose a username and a color, when failed redirect back to newplayer
+router.post('/newPlayer', function(req, res, next) {
+    // get data from fields
+    var playerName = req.body.playerName;
+    var playerColor = req.body.playerColor;
 
+    // check the data
+    req.checkBody('playerName', 'Fill in a username').notEmpty();
+    req.checkBody('playerColor', 'Pick a color!').notEmpty();
+
+    // collect errors
+    var errors = req.validationErrors();
+
+     if(errors){
+       res.render('newPlayer', {title: 'Create a new player', customJs: '/Javascripts/playerSelection.js', errors:errors});
+     }
+     else
+     {
+         //app.locals.player = logic.createPlayerByName(req.body.playerName, req.body.playerColor);
+         res.render('gameField', { title: 'new player' ,customJs:'/Javascripts/GameLogic.js'});
+     }
 });
 
 module.exports = router;
+
