3ab7a3b1c3e965dfc2fede5dc6e92d7565ff2c6c
danielschutte <dschutte@84-15.dhcp.windesheim.nl>
2017-03-23 10:10:15 +0100

    Geluid fixed

--- Game/clientGame/public/javascripts/indexApp.js
+++ Game/clientGame/public/javascripts/indexApp.js
@@ -1,11 +1,39 @@
 /**
  * Created by dschutte on 16-03-17.
+ *
  */
 
-function startOnClick(){
-    document.getElementById('explosion').play();
+// function startOnClick(){
+//     document.getElementById('explosion').play();
+//     location.href = "./game/newPlayer";
+//
+// }
 
-}
 
 
+// $('#start').click(function(){
+//     playSound(function() {
+//         alert("werkt");
+//         //location.href = "./game/newPlayer";
+//     })
+// });
+//
+// var playSound = function(callback) {
+//     document.getElementById('explosion').play(callback);
+// }
+
+(function(){
+
+    var sound = $('#explosion');
+
+    sound.on("ended", function(){
+        location.href = "./game/newPlayer";
+        })
+
+
+    $('#start').click(function(){
+        sound[0].play();
+    });
+
+}());
 
--- Game/clientGame/routes/index.js
+++ Game/clientGame/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'The Ultimate Stuck Shoot Game'});
+    res.render('index', { title: 'The Ultimate Stuck Shoot Game', customJs : '/Javascripts/indexApp.js'});
 });
 
 module.exports = router;
