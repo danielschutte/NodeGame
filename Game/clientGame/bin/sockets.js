45593077545a8d2d5c9fc41d7f9244c77aa91c12
danielschutte <dschutte@84-87.dhcp.windesheim.nl>
2017-03-28 12:04:30 +0200

    Player data to server works

--- Game/clientGame/bin/sockets.js
+++ Game/clientGame/bin/sockets.js
@@ -7,7 +7,6 @@ var SOCKET_LIST = {};
 var app = require('../routes/game').app;
 var logic = require('./logic');
 
-
 // all socket server side logic will take place here
 module.exports.listen = function(server){
 
@@ -19,9 +18,29 @@ module.exports.listen = function(server){
         socket.y = 0;
         SOCKET_LIST[socket.id] = socket;
         console.log('test works'+socket.id);
+
+
+        socket.on('data', function(data) {
+            // new Order({
+            //     playerName: data.playerName,
+            //     playerColor: data.playerColor
+            // }).save(function(err, docs) {
+            //     if(err) { return console.log("error"); }
+            //     // socket.emit('callback', {done: 'Done', data: data});
+            // });
+            // console.log('data:' + Order.data.playerName.toString() + ', ' + Order.data.playerColor.toString() );
+            console.log("data: name: " + data.playerName + ", color: " + data.playerColor);
+
+        });
+
+
     });
 
     return io;
+
+
+
+
 };
 setInterval(function () {
     var pack =[];
@@ -39,4 +58,5 @@ setInterval(function () {
         socket.emit('newPositions',pack);
     }
 },1000/25);
+
 
