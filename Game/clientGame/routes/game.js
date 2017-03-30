00fb6a1f7c0cb0bb97505d9db8c0535120ccd7c2
b93rn <lenardborn@gmail.com>
2017-03-30 10:57:47 +0200

    Merge with master

--- Game/clientGame/routes/game.js
+++ Game/clientGame/routes/game.js
@@ -3,11 +3,6 @@ var express = require('express');
 var logic = require('../bin/logic');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/GameField', function(req, res, next) {
-    res.render('GameField', { title: 'new player' ,customJs:'/Javascripts/GameLogic.js'});
-});
-
 // New player
 router.get('/newPlayer', function(req, res, next) {
     res.render('newPlayer', {title: 'Create a new player', customJs: '/Javascripts/newPlayer.js'});
@@ -31,14 +26,10 @@ router.post('/newPlayer', function(req, res, next) {
      }
      else
      {
-<<<<<<< HEAD
-         res.render('gameField', { title: 'new player' ,customJs:'/Javascripts/GameLogic.js', socket: 'https://cdnjs.cloudflare.com/ajax/libs/socket.io/1.7.3/socket.io.min.js'});
-=======
-         global.playerName = playerName;
+         global.playerName = plarName;
          global.playerColor = playerColor;
          //app.locals.player = logic.createPlayerByName(req.body.playerName, req.body.playerColor);
          res.render('gameField', { title: 'Game' ,customJs:'/Javascripts/GameLogic.js',socket: "https://cdnjs.cloudflare.com/ajax/libs/socket.io/1.7.3/socket.io.min.js"});
->>>>>>> origin/master
      }
 });
 
