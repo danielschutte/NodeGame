9b1b763843d295ea12bc8bc54a1bb8155aec68a1
b93rn <lenardborn@gmail.com>
2017-03-30 10:11:53 +0200

    Fixed layout handlebars

--- Game/clientGame/public/javascripts/newPlayer.js
+++ Game/clientGame/public/javascripts/newPlayer.js
@@ -46,9 +46,4 @@ function setColor(click){
 
 }
 
-// $('#continueButton').on('click', function(event){
-//     socket.emit('data',  {
-//         playerName: $('#pn').val(),
-//         playerColor: $('#pc').val()
-//     });
-// });
+
